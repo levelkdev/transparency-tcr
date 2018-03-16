11e4588cd9a3c50267721e5ddee0e6f80c073db4
Chris Whinfrey <cwhinfrey@gmail.com>
2018-03-16 10:28:54 -0400

    Initial commit

---
+++ test/parameterizer/challengeWinnerReward.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+/* global contract */
+
+contract('Parameterizer', () => {
+  describe('Function: challengeWinnerReward', () => {
+    it('should return the correct number of tokens to be granted to the winning entity in a challenge.');
+  });
+});
+
